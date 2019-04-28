8d2a1b267cb260a89d7be32a5fb3ae6de9465c77
alpha87 <jianxun2004@outlook.com>
2019-04-28 15:49:22 +0800

    使用相对路径

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ __doc__ = """助手相关配置"""
 
 # ------------- #
 # 识图路径
-beat_path = "F:\\\OnmyojiHelperGUI\\picture\\进攻.bmp"
-win_path = "F:\\\OnmyojiHelperGUI\\picture\\胜利.bmp"
-ko_path = "F:\\\OnmyojiHelperGUI\\picture\\挑战.bmp"
+beat_path = "picture\\进攻.bmp"
+win_path = "picture\\胜利.bmp"
+ko_path = "picture\\挑战.bmp"
 
 # ------------- #
 
 
 # 字库文件路径
-dict_path = "F:\\\OnmyojiHelperGUI\\dict\\dict_code.txt"
+dict_path = "dict\\dict_code.txt"
